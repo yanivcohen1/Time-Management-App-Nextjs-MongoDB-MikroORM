0f9a976fa45c64798719e95cfdd5ca1fec3f414c
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2026-01-11 01:51:56 +0200

    add embedded Screenshots

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,13 +3,14 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
    e2e: {
     baseUrl: 'http://localhost:3000',
-    supportFile: false,
     reporter: process.env.CYPRESS_REPORTER || 'spec',
     reporterOptions: {
       reportDir: 'cypress/reports',
       overwrite: true,
       html: true,
-      json: false
+      json: false,
+      embeddedScreenshots: true,
+      inlineAssets: true
     },
     async setupNodeEvents(on, config) {
       if (process.env.CYPRESS_REPORTER === 'cypress-mochawesome-reporter') {
